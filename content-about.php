361016205c775f45eaf4a7618f76c7647af77c20
isabelc <me@isabelcastillo.com>
2014-03-20 13:18:44 -0400

    added template tags for About images

--- content-about.php
+++ content-about.php
@@ -5,20 +5,26 @@
  * @package Crucible
  */
 ?>
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php crucible_about_top_image(); ?>
 	<header class="entry-header">
 		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h3><?php echo stripslashes_deep(get_option('smartestthemes_business_motto')); ?></h3>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
+		<?php $aboutcontent =  stripslashes_deep(get_option('smartestthemes_about_page'));
+		echo wpautop($aboutcontent); 
+		the_content();
+		crucible_about_bottom_image();
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . __( 'Pages:', 'crucible' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'crucible' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' ); ?>
+	<?php edit_post_link( __( 'Edit', 'crucible' ), '<footer class="entry-footer"><span class="edit-link">', '</span></footer>' );
+if(get_option('smartestthemes_stop_smartshare') == 'false') {
+	echo smartestthemes_share();
+} ?>
 </article><!-- #post-## -->
