623d99efa8016ab8e6fc9e766b1dc7b87c38c0b3
isabelc <me@isabelcastillo.com>
2014-04-03 19:25:59 -0400

    include new filenames

--- functions.php
+++ functions.php
@@ -7,15 +7,15 @@
 /* Please refrain from editing this section */
 /*-----------------------------------------------------------------------------------*/
 
-include( dirname( __FILE__ ) . '/inc/license-handler.php' );
+include( dirname( __FILE__ ) . '/inc/updater.php' );
 // Smartest Themes Business Framework
 require_once (TEMPLATEPATH . '/business-framework/admin-init.php'); 
 // Set path to theme specific functions
 $incpath = TEMPLATEPATH . '/inc/';
 // Theme specific functionality
-require_once ($incpath . 'theme-options.php'); 	// Options panel settings
+require_once ($incpath . 'options.php'); 	// Options panel settings
 require_once ($incpath . 'theme-functions.php'); 	// theme functions
-require_once ($incpath . 'theme-enqueue.php');	// Load javascript and styles
+require_once ($incpath . 'enqueue.php');	// Load javascript and styles
 require_once ($incpath . 'fontface.php');		// Load font stylesheets
 /*-----------------------------------------------------------------------------------*/
 /* End Crucible Theme Functions - You can add custom functions below, or better yet, place them in a child theme. */
