57a938906aa2c3abf02bb7b40bbd71800bc12c29
isabelc <me@isabelcastillo.com>
2014-04-07 18:01:08 -0400

    change nav_fallback to crucible_nav_fallback

--- header.php
+++ header.php
@@ -41,10 +41,7 @@ do_action( 'crucible_social_buttons' ); // @todo here or in footer, decide per t
 			<h1 class="menu-toggle"><?php _e( 'Menu', 'crucible' ); ?></h1>
 			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'crucible' ); ?></a>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary','fallback_cb' => 'smartestthemes_nav_fallback', 'items_wrap' => '<ul class="%2$s">%3$s</ul>' ) );
-
-// @todo  search in frame and elsewhere to change 'smartestb_mainnav_fallback' to 'smartestthemes_nav_fallback'
-?>
+			<?php wp_nav_menu( array( 'theme_location' => 'primary','fallback_cb' => 'crucible_nav_fallback', 'items_wrap' => '<ul class="%2$s">%3$s</ul>' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
