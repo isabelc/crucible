724d26590da1fd43410dbb5448ffc723323230a5
isabelc <me@isabelcastillo.com>
2014-04-14 12:40:05 -0400

    fix missing endif

--- page.php
+++ page.php
@@ -35,12 +35,11 @@ if (is_page(get_option('smartestthemes_contact_page_id'))){
 						get_template_part( 'content', 'about' );
 					else : 
 						get_template_part( 'content', 'page' );
-				?>
-
-				<?php
-					// If comments are open or we have at least one comment, load up the comment template
-					if ( comments_open() || '0' != get_comments_number() ) :
-						comments_template();
+							
+						// If comments are open or we have at least one comment, load up the comment template
+						if ( comments_open() || '0' != get_comments_number() ) :
+							comments_template();
+						endif;
 					endif;
 				?>
 
