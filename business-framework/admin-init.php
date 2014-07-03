0a6e9c758880f1ffee4847bd0f177b491324e2b4
isabelc <me@isabelcastillo.com>
2014-07-03 01:54:11 -0400

    remove parenthesis from include_once

--- business-framework/admin-init.php
+++ business-framework/admin-init.php
@@ -7,15 +7,15 @@
  * @version    0.0.1
  */
 if( class_exists( 'Quick_Business_Website' ) ) {
-	require_once (ABSPATH .'/wp-admin/includes/plugin.php');
+	require_once ABSPATH .'/wp-admin/includes/plugin.php';
 	deactivate_plugins( '/quick-business-website/quick-business-website.php' );
 	$msg =  '<strong>' . __( 'You cannot activate a "Smartest Theme"', 'crucible') . '</strong> ' . __( 'when using the <strong>Quick Business Website</strong> plugin because they clash. The Quick Business Website plugin is being deactivated now so you may use your <strong>Smartest Theme</strong>. Please go back and activate the theme again.', 'crucible');
 	wp_die($msg, 'Theme Clashes With Plugin', array(back_link => true));
 } else {
 	$path = TEMPLATEPATH .'/business-framework/';
-	require_once ($path .'admin-interface.php');
-	require_once ($path .'smartest-functions.php');
-	require_once ($path .'modules/contact/contact.php');
+	require_once $path .'admin-interface.php';
+	require_once $path .'smartest-functions.php';
+	require_once $path .'modules/contact/contact.php';
 	include_once $path .'widgets/announcements.php';
 	include_once $path .'widgets/all-services.php';
 	include_once $path .'widgets/featured-services.php';
