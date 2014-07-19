9beda8af9f901cbb1dac07920e3b5bd148a619f2
isabelc <me@isabelcastillo.com>
2014-07-19 14:12:13 -0400

    do not enqueue responsive.js

--- business-framework/smartest-functions.php
+++ business-framework/smartest-functions.php
@@ -986,11 +986,7 @@ add_action( 'wp_before_admin_bar_render', 'smartestthemes_admin_bar' );
 function smartest_framework_enq() {
 	wp_register_style( 'frame', get_template_directory_uri().'/business-framework/css/frame.css');
 	wp_enqueue_style( 'frame' );// @todo sync with style sheet.
-	wp_register_script('responsive', get_template_directory_uri().'/business-framework/js/responsive.js', array('jquery'), false, true);// @todo possibly make inline in wp-head
-	// not on reviews page
-	if( !is_page( get_option('smartestthemes_reviews_page_id') ) ) {	
-		wp_enqueue_script('responsive');
-	}
+
 	wp_enqueue_script('retina', get_template_directory_uri().'/business-framework/js/retina-1.1.0.min.js', array(), false, true );
 
 }
