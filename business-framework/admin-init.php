d5c188824795dc78bbab08b9f10bf08a08f82b3f
isabelc <me@isabelcastillo.com>
2014-09-17 11:24:31 -0400

    add comments about theme and plugin clash

--- business-framework/admin-init.php
+++ business-framework/admin-init.php
@@ -6,7 +6,7 @@
  * @author     Smartest Themes <isa@smartestthemes.com>
  * @version    1.0-beta-1
  */
-if( class_exists( 'Quick_Business_Website' ) ) {
+if( class_exists( 'Quick_Business_Website' ) ) {// @todo some new way... if i even still need??
 	require_once ABSPATH .'/wp-admin/includes/plugin.php';
 	deactivate_plugins( '/quick-business-website/quick-business-website.php' );
 	$msg =  '<strong>' . __( 'You cannot activate a "Smartest Theme"', 'crucible') . '</strong> ' . __( 'when using the <strong>Quick Business Website</strong> plugin because they clash. The Quick Business Website plugin is being deactivated now so you may use your <strong>Smartest Theme</strong>. Please go back and activate the theme again.', 'crucible');
@@ -16,10 +16,10 @@ if( class_exists( 'Quick_Business_Website' ) ) {
 	require_once $path . 'admin-interface.php';
 	require_once $path . 'smartest-functions.php';
 	require_once $path . 'modules/contact/contact.php';
-	include_once $path . 'widgets/announcements.php';
 	include_once $path . 'widgets/all-services.php';
 	include_once $path . 'widgets/featured-services.php';
-	include_once $path . 'widgets/staff.php';
+	include_once $path . 'widgets/announcements.php';
 	include_once $path . 'widgets/featured-announcements.php';
+	include_once $path . 'widgets/staff.php';
 }
 ?>
