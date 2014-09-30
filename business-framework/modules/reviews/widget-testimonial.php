eeed3d01b8ceed1f945240778cf1cac432453c4b
isabelc <me@isabelcastillo.com>
2014-09-30 11:47:57 -0400

    Check if testimonials class exists first.

--- business-framework/modules/reviews/widget-testimonial.php
+++ business-framework/modules/reviews/widget-testimonial.php
@@ -7,92 +7,95 @@
  * @extends 	WP_Widget
  */
 
-class SmartestReviewsTestimonial extends WP_Widget {
-	/**
-	 * Register widget with WordPress.
-	 */
-	public function __construct() {
-		parent::__construct(
-	 		'smartest_reviews_testimonial',
-			'Smartest Reviews Testimonial',
-			array( 'description' => __( 'Display a random review as a testimonial.', 'crucible' ), )
-		);
-	}
-	/**
-	 * Front-end display of widget.
-	 * @param array $args     Widget arguments.
-	 * @param array $instance Saved values from database.
-	 */
-	public function widget( $args, $instance ) {
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Testimonials', 'crucible' ) : $instance['title'], $instance, $this->id_base );
-		$number = isset( $instance['number'] ) ? $instance['number'] : '';
-		echo $args['before_widget']; ?>
-		<h3 class="widget-title"><?php echo $title; ?></h3>
-		<?php
-		/** 
-		* pull reviews from smartest reviews table 
-		*/
+ if ( ! class_exists( 'SmartestReviewsTestimonial' ) ) {
+	class SmartestReviewsTestimonial extends WP_Widget {
+		/**
+		 * Register widget with WordPress.
+		 */
+		public function __construct() {
+			parent::__construct(
+				'smartest_reviews_testimonial',
+				'Smartest Reviews Testimonial',
+				array( 'description' => __( 'Display a random review as a testimonial.', 'crucible' ), )
+			);
+		}
+		/**
+		 * Front-end display of widget.
+		 * @param array $args     Widget arguments.
+		 * @param array $instance Saved values from database.
+		 */
+		public function widget( $args, $instance ) {
+			$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Testimonials', 'crucible' ) : $instance['title'], $instance, $this->id_base );
+			$number = isset( $instance['number'] ) ? $instance['number'] : '';
+			echo $args['before_widget']; ?>
+			<h3 class="widget-title"><?php echo $title; ?></h3>
+			<?php
+			/** 
+			* pull reviews from smartest reviews table 
+			*/
 
-		global $wpdb;
+			global $wpdb;
 
-		$reviews_pageurl = get_permalink(get_option('smartestthemes_reviews_page_id'));
-		$pre = $wpdb->base_prefix;
+			$reviews_pageurl = get_permalink(get_option('smartestthemes_reviews_page_id'));
+			$pre = $wpdb->base_prefix;
 
-		if ( is_multisite() ) { 
-			global $blog_id;
-			$bi = get_current_blog_id();
-			
-			$pre2 = $pre . $bi . '_st_reviews';
-		} else {
-			// not Multisite
-			$pre2 = $pre . 'st_reviews';
+			if ( is_multisite() ) { 
+				global $blog_id;
+				$bi = get_current_blog_id();
+				
+				$pre2 = $pre . $bi . '_st_reviews';
+			} else {
+				// not Multisite
+				$pre2 = $pre . 'st_reviews';
+			}
+
+			$number_testimonials = ! empty( $number ) ? $number : 1;
+			$getreviews = $wpdb->get_results("SELECT review_text FROM $pre2 WHERE status = 1 LIMIT 0,$number_testimonials");
+		
+			if ( empty( $getreviews ) ) {
+					//no review yet, lure them to leave one
+					?>
+					<p><?php _e( 'Be the first to', 'crucible' ); echo ' '; ?> <a href="<?php echo $reviews_pageurl; ?>"><?php _e( 'leave a review...', 'crucible' ); ?></a></p>
+			<?php } else {
+					foreach ( $getreviews as $getreview ) { ?>
+						<blockquote><?php echo wp_trim_words( $getreview->review_text, 20); ?></blockquote><br />
+					<?php } ?>
+					<a href="<?php echo $reviews_pageurl; ?>"><?php _e('More...', 'crucible'); ?></a>
+			<?php }
+			echo $args['after_widget'];
+		}// end widget
+
+		/**
+		 * Sanitize widget form values as they are saved.
+		 * @param array $new_instance Values just sent to be saved.
+		 * @param array $old_instance Previously saved values from database.
+		 * @return array Updated safe values to be saved.
+		 */
+		public function update( $new_instance, $old_instance ) {
+			$instance = array();
+			$instance['title'] = strip_tags($new_instance['title'] );
+			$instance['number'] = strip_tags( $new_instance['number'] );
+			return $instance;
 		}
 
-		$number_testimonials = ! empty( $number ) ? $number : 1;
-		$getreviews = $wpdb->get_results("SELECT review_text FROM $pre2 WHERE status = 1 LIMIT 0,$number_testimonials");
-	
-		if ( empty( $getreviews ) ) {
-				//no review yet, lure them to leave one
-				?>
-				<p><?php _e( 'Be the first to', 'crucible' ); echo ' '; ?> <a href="<?php echo $reviews_pageurl; ?>"><?php _e( 'leave a review...', 'crucible' ); ?></a></p>
-		<?php } else {
-				foreach ( $getreviews as $getreview ) { ?>
-					<blockquote><?php echo wp_trim_words( $getreview->review_text, 20); ?></blockquote><br />
-				<?php } ?>
-				<a href="<?php echo $reviews_pageurl; ?>"><?php _e('More...', 'crucible'); ?></a>
-		<?php }
-		echo $args['after_widget'];
-	}// end widget
-
-	/**
-	 * Sanitize widget form values as they are saved.
-	 * @param array $new_instance Values just sent to be saved.
-	 * @param array $old_instance Previously saved values from database.
-	 * @return array Updated safe values to be saved.
-	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = array();
-		$instance['title'] = strip_tags($new_instance['title'] );
-		$instance['number'] = strip_tags( $new_instance['number'] );
-		return $instance;
-	}
-
-	/**
-	 * Back-end widget form.
-	 * @param array $instance Previously saved values from database.
-	 */
-	public function form( $instance ) {
-		$title = isset( $instance[ 'title' ] ) ? $instance[ 'title' ] : __( 'Testimonials', 'crucible' );
-		$number = isset( $instance[ 'number' ] ) ? $instance[ 'number' ] : 1;
-		?>
+		/**
+		 * Back-end widget form.
+		 * @param array $instance Previously saved values from database.
+		 */
+		public function form( $instance ) {
+			$title = isset( $instance[ 'title' ] ) ? $instance[ 'title' ] : __( 'Testimonials', 'crucible' );
+			$number = isset( $instance[ 'number' ] ) ? $instance[ 'number' ] : 1;
+			?>
+			<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'crucible' ); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			</p>
 		<p>
-		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'crucible' ); ?></label> 
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+			<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'How many testimonials to show:', 'crucible' ); ?></label> 
+			<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" value="<?php echo esc_attr( $number ); ?>" />
 		</p>
-	<p>
-		<label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'How many testimonials to show:', 'crucible' ); ?></label> 
-		<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" value="<?php echo esc_attr( $number ); ?>" />
-	</p>
-		<?php 
+			<?php 
+		}
 	}
-} ?>
+}
+?>
